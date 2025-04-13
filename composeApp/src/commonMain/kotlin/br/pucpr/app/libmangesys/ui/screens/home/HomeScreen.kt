4d10abc6f525b62dd31c3eaf3595a599b9c01764
thiago-you <thiago.youx@gmail.com>
2025-04-13 17:45:58 -0300

    update home interface

--- composeApp/src/commonMain/kotlin/br/pucpr/app/libmangesys/ui/screens/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/br/pucpr/app/libmangesys/ui/screens/home/HomeScreen.kt
@@ -10,15 +10,32 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -48,71 +65,69 @@ fun HomeScreenPreview() {
 }
 
 @Composable
+@OptIn(ExperimentalMaterial3Api::class)
 fun HomeScreenContent(intent: HomeIntent) {
     val navigator = LocalNavigator.current
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Black),
-        contentAlignment = Alignment.Center
-    ) {
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Spacer(modifier = Modifier.weight(1f))
-
-            Text(
-                modifier = Modifier.padding(bottom = 32.dp)
-                    .padding(horizontal = 16.dp),
-                text = intent.getTitleText(),
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.White,
-                textAlign = TextAlign.Center
-            )
-
-            Spacer(modifier = Modifier.weight(1f))
-
+    Scaffold(
+        content = { padding ->
             Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 60.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(16.dp)
+                    .fillMaxSize()
+                    .background(color = Color.LightGray)
+                    .padding(padding)
+                    .padding(24.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                ButtonComponent(text = intent.getBtnUsersText()) {
-                    navigate(navigator, intent.onBtnUsersClick())
-                }
-                ButtonComponent(text = intent.getBtnBooksText()) {
-                    navigate(navigator, intent.onBtnBooksClick())
-                }
-                ButtonComponent(text = intent.getBtnBorrowsText()) {
-                    navigate(navigator, intent.onBtnBorrowsClick())
+                Card(
+                    elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+                    colors = CardDefaults.cardColors(contentColor = Color.White),
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .padding(24.dp)
+                            .fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        Text(
+                            text = intent.getTitleText(),
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.Black,
+                            textAlign = TextAlign.Center
+                        )
+
+                        Spacer(Modifier.size(32.dp))
+
+                        ButtonWithIcon(intent.getBtnUsersText(), Icons.Default.Person) {
+                            navigate(navigator, intent.onBtnUsersClick())
+                        }
+                        ButtonWithIcon(intent.getBtnBooksText(), Icons.Filled.Favorite) {
+                            navigate(navigator, intent.onBtnUsersClick())
+                        }
+                        ButtonWithIcon(intent.getBtnBorrowsText(), Icons.AutoMirrored.Filled.List) {
+                            navigate(navigator, intent.onBtnUsersClick())
+                        }
+                    }
                 }
             }
         }
-    }
+    )
 }
 
 @Composable
-fun ButtonComponent(text: String, onClick: () -> Unit) {
+fun ButtonWithIcon(text: String, icon: ImageVector, onClick: () -> Unit) {
     Button(
         onClick = onClick,
-        modifier = Modifier
-            .fillMaxWidth(0.8f)
-            .height(48.dp),
-        colors = ButtonDefaults.buttonColors(containerColor = Color.Black),
-        shape = RoundedCornerShape(8.dp),
-        border = BorderStroke(1.dp, Color.White)
+        modifier = Modifier.fillMaxWidth(),
+        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary)
     ) {
-        Text(
-            text = text,
-            fontSize = 16.sp,
-            color = Color.White
-        )
+        Icon(icon, contentDescription = null)
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(text)
     }
 }
 
