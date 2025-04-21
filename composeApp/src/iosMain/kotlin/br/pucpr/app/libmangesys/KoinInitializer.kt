51ae71438d96517762831964ba894316fd6adf4b
thiago-you <thiago.youx@gmail.com>
2025-04-21 20:09:48 -0300

    fix init di

--- composeApp/src/iosMain/kotlin/br/pucpr/app/libmangesys/KoinInitializer.kt
+++ composeApp/src/iosMain/kotlin/br/pucpr/app/libmangesys/KoinInitializer.kt
@@ -1,11 +1,9 @@
 package br.pucpr.app.libmangesys
 
-import org.koin.core.context.startKoin
-
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 actual class KoinInitializer {
     actual fun init() {
-        startKoin {
+        initKoin {
             modules(appModule, viewModelModule)
         }
     }
